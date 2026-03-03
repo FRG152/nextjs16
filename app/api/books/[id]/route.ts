e47f3708686ff6886cfcdca5c3264dd9d9c7d679
Flavio Roman Goncalez <romanflavio3@hotmail.com>
2026-03-02 21:26:44 -0300

    backend in nextjs

--- app/api/books/[id]/route.ts
+++ app/api/books/[id]/route.ts
@@ -1,16 +1,16 @@
 import books from "../../db";
 
-export async function PUT(request: Request, context: {params: {id: string}}) {
-    const id = +context.params.id;
+export async function PUT(request: Request, context: {params: Promise<{id: string}>}) {
+    const { id }  = await context.params;
     const book = await request.json();
-    const index = books.findIndex((b) => b.id === id);
+    const index = books.findIndex((b) => b.id === +id);
     books[index] = book;
     return Response.json(books)
 }
 
-export async function DELETE(request: Request, context: {params: {id: string}}) {
-    const id = +context.params.id;
-    const index = books.findIndex((b) => b.id === id);
+export async function DELETE(request: Request, context: {params: Promise<{id: string}>}) {
+    const { id } = await context.params;
+    const index = books.findIndex((b) => b.id === +id);
     books.splice(index, 1);
     return Response.json(books)
 }
